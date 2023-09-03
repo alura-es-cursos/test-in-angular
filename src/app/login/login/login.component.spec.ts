55608ec4f92b4454fe623c5d2fc5f7a21a8c12ec
guaigua <gerguaigua@gmail.com>
2023-09-02 23:03:27 -0400

    Feat :sparkless: TestBed

--- src/app/login/login/login.component.spec.ts
+++ src/app/login/login/login.component.spec.ts
@@ -7,14 +7,29 @@ describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
-  beforeEach(() => {
+  beforeAll(() => {
     TestBed.configureTestingModule({
       declarations: [LoginComponent],
       imports: [FormsModule, ReactiveFormsModule]
     });
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 
   it('should create', () => {
