e2da169a4fbd7babd67c81e97d8e74aee56c841d
guaigua <gerguaigua@gmail.com>
2023-09-05 23:51:30 -0400

    Feat :sparkles: Aula 3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('test-in-angular app is running!');
+    expect(compiled.querySelector('span')?.textContent).toContain('test-in-angular app is running!');
   });
 });
